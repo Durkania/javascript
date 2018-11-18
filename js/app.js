abd6ed6e8fb2963acf41e0349b2c98c4e6f254d8
Jhonatan <alogenuss@gmail.com>
2018-11-18 18:41:09 -0500

    Nuevo documento

--- js/app.js
+++ js/app.js
@@ -1,21 +1,15 @@
 var Calculadora = {
-    keys: {'0':'0','1':'1','2':'2','3':'3','4':'4','5':'5','6':'6','7':'7','8':'8','9':'9','punto':'.',
-	'sign':'s','mas':'+','menos':'-','por':'*','dividido':'/','raiz':'r','igual':'=','on':'o'},
-	ids: {'0':'0','1':'1','2':'2','3':'3','4':'4','5':'5','6':'6','7':'7','8':'8','9':'9','.':'punto',
-	's':'sign','+':'mas','-':'menos','*':'por','/':'dividido','r':'raiz','=':'igual','o':'on'},
-	pantalla: document.getElementById('display'),
-	pantallaGuardada: '',
-	operacionActual: '',
-	operacionAnterior: '',
-	init: function() {
-		document.onmousedown = this.manejadorMouseDown;
-		document.onmouseup = this.manejadorMouseUp;
-		document.onkeydown = this.manejadorKeyDown;
-		document.onkeyup = this.manejadorKeyUp;
+	init: function(){
+		this.iniciarVariables()
+		this.reinicioDisplay()
 	},
-}
-
-window.onload = function() {
-	Calculadora.init();5
-	console.log('calculadora lista')
-};
+	iniciarVariables: function(){
+		var self = this
+		var classname = document.getElementsByClassName("tecla");
+	 for (var i = 0; i < classname.length; i++) {
+	    (function () {
+	        var boxa = classname[i].id;
+	        if(boxa!='mas'){
+	        classname[i].addEventListener("mouseover", function() {  self.mouseover(boxa); }, false);
+	         classname[i].addEventListener("mouseout", function() {  self.mouseout(boxa); }, false);
+	     }
